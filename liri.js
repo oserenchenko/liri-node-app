170ca0b17f224983ca5465d718a9a6ed78f7ad77
Olga <oserenchenko@gmail.com>
2018-07-28 22:49:45 -0600

    spotify command

--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
 var keys = require('./keys.js');
-// var Spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var command = process.argv[2];
 
-
+// ----------------------Functions for Commands----------------------------------------
 function twitterGet() {
   var params = {
     screen_name: 'nodejs'
@@ -25,12 +25,39 @@ function twitterGet() {
       console.log('\t' + 'Created: ' + dateSplit[0] + ' ' + dateSplit[1] + ' ' + dateSplit[2] + ' ' + dateSplit[3]);
     }
   });
-}
+};
 
+function spotifyGet(music) {
+  spotify.search({
+    type: 'track',
+    query: music
+  }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    var songObj = data.tracks.items[0];
+    console.log('Song: ' + songObj.name);
+    console.log('Artist: ' + songObj.album.artists[0].name);
+    console.log('Album: ' + songObj.album.name);
+    console.log('Spotify Link: ' + songObj.external_urls.spotify);
+  });
+};
+
+
+// spotifyGet('Helter Skelter');
+
+
+// ----------------------runs LIRI----------------------------------------
 if (command === `my-tweets`) {
   twitterGet();
 } else if (command === `spotify-this-song`) {
-
+  var song = process.argv[3];
+  if (song) {
+    spotifyGet(song);
+  } else {
+    var defaultSong = "The Sign";
+    spotifyGet(defaultSong);
+  }
 } else if (command === `movie-this`) {
 
 } else if (command === `do-what-it-says`) {
