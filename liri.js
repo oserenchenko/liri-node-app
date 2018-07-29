461014d202f544a9feb98b23bd72d934d782e4b1
Olga <oserenchenko@gmail.com>
2018-07-28 23:22:22 -0600

    added omdb function and reading outside text file

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
+//setting up all global variables, keys, tokens, and npm's
 require("dotenv").config();
-
+var request = require("request");
+var fs = require("fs");
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
@@ -8,6 +10,7 @@ var client = new Twitter(keys.twitter);
 var command = process.argv[2];
 
 // ----------------------Functions for Commands----------------------------------------
+//Twitter Function: gets the last 20 tweets from my Twitter account
 function twitterGet() {
   var params = {
     screen_name: 'nodejs'
@@ -27,6 +30,7 @@ function twitterGet() {
   });
 };
 
+//Spotify Function: looks up the song, artist, album, and link for the song specified by user
 function spotifyGet(music) {
   spotify.search({
     type: 'track',
@@ -43,9 +47,23 @@ function spotifyGet(music) {
   });
 };
 
-
-// spotifyGet('Helter Skelter');
-
+//OMDB Function: looks up the name, year, rating, country, language, plot, and actors for the movie specified by user
+function movieGet(movieChoice) {
+  request("http://www.omdbapi.com/?t=" + movieChoice + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log('Movie: ' + JSON.parse(body).Title);
+      console.log('Year: ' + JSON.parse(body).Year);
+      console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
+      console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value);
+      console.log('Country: ' + JSON.parse(body).Country);
+      console.log('Language: ' + JSON.parse(body).Language);
+      console.log('Plot: ' + JSON.parse(body).Plot);
+      console.log('Actors: ' + JSON.parse(body).Actors);
+    } else {
+      console.log(error);
+    }
+  });
+};
 
 // ----------------------runs LIRI----------------------------------------
 if (command === `my-tweets`) {
@@ -59,9 +77,32 @@ if (command === `my-tweets`) {
     spotifyGet(defaultSong);
   }
 } else if (command === `movie-this`) {
-
+  var movie = process.argv[3];
+  if (movie) {
+    movieGet(movie);
+  } else {
+    var defaultMovie = "Mr. Nobody";
+    movieGet(defaultMovie);
+  }
 } else if (command === `do-what-it-says`) {
-
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    } else {
+      var dataArr = data.split(",");
+      var whatToDo = dataArr[0];
+      var details = dataArr[1];
+      if (whatToDo === `my-tweets`) {
+        twitterGet();
+      } else if (whatToDo === `spotify-this-song`) {
+        spotifyGet(details);
+      } else if (whatToDo === `movie-this`) {
+        movieGet(details);
+      } else {
+        console.log("That is not a command that I know, sorry!")
+      }
+    }
+  });
 } else {
   console.log("That is not a command that I know, sorry!")
 };
